ab28add7afa5c9cb03d97895b60903fc96515341
Miraclx <omiraculous@gmail.com>
2020-06-26 07:10:21 +0100

    Update test content

--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,11 @@ const cStringd = require('../dist');
 
 test('basic test', () => {
   const result = cStringd(
-    ':{bgcolor(black)}:{color(red)}My :{color(cyan)}name :{color(yellow)}is :{color(underline)}:{name} :{color:reset}',
+    ':{bgcolor(black)}:{color(red)}My :{color(cyan)}name :{color(yellow)}is :{color(underline)}:{name}:{color:reset}',
     {
       name: 'stringd_colors',
     },
   );
   console.log(`Result: ${result}`);
-  expect(result).toBe('\u001b[40m\u001b[31mMy \u001b[36mname \u001b[33mis \u001b[4mstringd_colors \u001b[0m');
+  expect(result).toBe('\u001b[40m\u001b[31mMy \u001b[36mname \u001b[33mis \u001b[4mstringd_colors\u001b[0m');
 });
